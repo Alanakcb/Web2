f22036bfdbd152bbf3d3bdf57625e8d39527e827
Alanakcb <alanacantuaria@gmail.com>
2022-05-03 08:28:35 -0300

    03.05

--- controller/Form.php
+++ controller/Form.php
@@ -2,6 +2,7 @@
 class Form
 {
   private $message = "";
+  private $error = "";
   public function __construct()
   {
     Transaction::open();
@@ -17,39 +18,75 @@ class Form
   }
   public function salvar()
   {
-    if(isset($_POST["tarefa"]) && isset($_POST["data"]) && isset($_POST["horario"])){
+    if (isset($_POST["tarefa"]) && isset($_POST["data"]) && isset($_POST["horario"])) {
       try {
         $conexao = Transaction::get();
         $agenda = new Crud("agenda");
         $tarefa = $conexao->quote($_POST["tarefa"]);
         $data = $conexao->quote($_POST["data"]);
         $horario = $conexao->quote($_POST["horario"]);
-        $resultado = $agenda->insert("tarefa, data, horario", "$tarefa, $data, $horario");
+        if (empty($_POST["id"])) {
+          $agenda->insert(
+            "tarefa, data, horario",
+            "$tarefa, $data, $horario"
+          );
+        } else {
+          $id = $conexao->quote($_POST["id"]);
+          $agenda->update(
+            "tarefa = $tarefa, data = $data, horario = $horario",
+            "id = $id"
+          );
+        }
+        $this->message = $agenda->getMessage();
+        $this->error = $agenda->getError();
       } catch (Exception $e) {
-        echo $e->getMessage();
+        $this->message = $e->getMessage();
+        $this->error = true;
       }
+    } else {
+      $this->message = "Campos não informados!";
+      $this->error = true;
     }
   }
-  public function editar(){
+  public function editar()
+  {
     if (isset($_GET["id"])) {
       try {
         $conexao = Transaction::get();
         $id = $conexao->quote($_GET["id"]);
         $agenda = new Crud("agenda");
         $resultado = $agenda->select("*", "id = $id");
-        $form = new Template("view/form.html");
-        foreach ($resultado[0] as $cod => $valor) {
-          $form->set($cod, $valor);
+        if (!$agenda->getError()) {
+          $form = new Template("view/form.html");
+          foreach ($resultado[0] as $cod => $horario) {
+            $form->set($cod, $horario);
+          }
+          $this->message = $form->saida();
+        } else {
+          $this->message = $agenda->getMessage();
+          $this->error = true;
         }
-        $this->message = $form->saida();
       } catch (Exception $e) {
-        echo $e->getMessage();
+        $this->message = $e->getMessage();
+        $this->error = true;
       }
     }
   }
   public function getMessage()
   {
-    return $this->message;
+    if (is_string($this->error)) {
+      return $this->message;
+    } else {
+      $msg = new Template("view/msg.html");
+      if ($this->error) {
+        $msg->set("cor", "danger");
+      } else {
+        $msg->set("cor", "success");
+      }
+      $msg->set("msg", $this->message);
+      $msg->set("uri", "?class=Tabela");
+      return $msg->saida();
+    }
   }
   public function __destruct()
   {
--- controller/Tabela.php
+++ controller/Tabela.php
@@ -2,30 +2,47 @@
 class Tabela
 {
   private $message = "";
+  private $error = "";
   public function __construct()
   {
     Transaction::open();
   }
   public function controller()
   {
-    Transaction::get();
-    $agenda = new Crud("agenda");
-    $resultado = $agenda->select();
-    $tabela = new Template("view/tabela.html");
-    if (is_array($resultado)) {
-      $tabela->set("linha", $resultado);
-      $this->message = $tabela->saida();
+    try {
+      Transaction::get();
+      $agenda = new Crud("agenda");
+      $resultado = $agenda->select();
+      $tabela = new Template("view/tabela.html");
+      if (is_array($resultado)) {
+        $tabela->set("linha", $resultado);
+        $this->message = $tabela->saida();
+      } else {
+        $this->message = $agenda->getMessage();
+        $this->error = $agenda->getError();
+      }
+    } catch (Exception $e) {
+      $this->message = $e->getMessage();
+      $this->error = true;
     }
   }
   public function remover()
   {
-    try {
-      $conexao = Transaction::get();
-      $id = $conexao->quote($_GET["id"]);
-      $agenda = new Crud("agenda");
-      $resultado = $agenda->delete("id = $id");
-    } catch (Exception $e) {
-      echo $e->getMessage();
+    if (isset($_GET["id"])) {
+      try {
+        $conexao = Transaction::get();
+        $id = $conexao->quote($_GET["id"]);
+        $agenda = new Crud("agenda");
+        $agenda->delete("id = $id");
+        $this->message = $agenda->getMessage();
+        $this->error = $agenda->getError();
+      } catch (Exception $e) {
+        $this->message = $e->getMessage();
+        $this->error = true;
+      }
+    } else {
+      $this->message = "Faltando parâmetro!";
+      $this->error = true;
     }
   }
   public function getMessage()
